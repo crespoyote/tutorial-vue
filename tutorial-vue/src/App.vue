61d5375811a87b7dad1ccd01be639e1748c99963
Alfonso Crespo <alfonsocrespobelda@gmail.com>
2024-03-08 10:49:25 +0100

    semana 2 archivos.

--- tutorial-vue/src/App.vue
+++ tutorial-vue/src/App.vue
@@ -27,6 +27,7 @@
 
 <script>
   // Importación del componente "TablaPersonas"
+  const myVar = import.meta.env.VITE_DJANGOURL;
   import TablaPersonas from '@/components/TablaPersonas.vue'
   import FormularioPersona from '@/components/FormularioPersona.vue'
   import { ref, onMounted } from 'vue';
@@ -52,7 +53,7 @@
       const listadoPersonas = async () => {
         // Metodo para obtener un listado de personas
         try {
-          const response = await fetch('http://127.0.0.1:8001/api/v1/personas/');
+          const response = await fetch(myVar + '/api/v1/personas/');
           personas.value = await response.json();
         } catch (error) {
           console.error(error);
@@ -61,7 +62,7 @@
 
       const agregarPersona = async (persona) => {
         try {
-          const response = await fetch('http://127.0.0.1:8001/api/v1/personas/', {
+          const response = await fetch(myVar + '/api/v1/personas/', {
             method: 'POST',
             body: JSON.stringify(persona),
             headers: { 'Content-type': 'application/json; charset=UTF-8' },
@@ -78,7 +79,7 @@
       const eliminarPersona = async (persona_id) => {
         // Metodo para eliminar una persona
         try {
-          await fetch('http://127.0.0.1:8001/api/v1/personas/'+persona_id+'/', {
+          await fetch(myVar + '/api/v1/personas/'+persona_id+'/', {
             method: "DELETE"
           });
           personas.value= personas.value.filter(u => u.id !== persona_id);
@@ -90,7 +91,7 @@
       const actualizarPersona = async (id, personaActualizada) => {
         // Metodo para actualizar una persona
         try {
-          const response = await fetch('http://127.0.0.1:8001/api/v1/personas/'+personaActualizada.id+'/', {
+          const response = await fetch(myVar + '/api/v1/personas/'+personaActualizada.id+'/', {
             method: 'PUT',
             body: JSON.stringify(personaActualizada),
             headers: { 'Content-type': 'application/json; charset=UTF-8' },
