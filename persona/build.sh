c97f4261f09aec8c7c4e170c20cccbdb2a57efae
Alfonso Crespo <alfonsocrespobelda@gmail.com>
2024-03-14 16:58:39 +0100

    build.h

---
+++ persona/build.sh
@@ -0,0 +1,10 @@
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+python3 manage.py makemigrations
+
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
+
+python manage.py createsuperuser
